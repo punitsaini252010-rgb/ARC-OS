c2de91740bba22a9cb9c7aa861b22495eeebe17f
punitsaini252010-rgb <punitsaini252010@gmail.com>
2026-04-01 16:00:51 +0530

    Add authentication middleware for public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,11 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // Public routes are the only ones people can see without logging in
+  publicRoutes: ["/", "/api/webhooks(.*)"], 
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
 
