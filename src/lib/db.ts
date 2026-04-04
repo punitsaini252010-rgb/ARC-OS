3d8c4fff6fb79a355f5eaa1cb4721c1747c3d0ed
punitsaini252010-rgb <punitsaini252010@gmail.com>
2026-04-04 14:59:37 +0530

    Refactor PrismaClient initialization and global variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export const db = globalThis.prisma ?? prismaClientSingleton()
 
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
